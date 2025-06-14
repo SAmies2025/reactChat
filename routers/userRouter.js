a4ceff4f0b6d19f6ecd28014c1ed254312fa6c94
Sarah Amies <sarah.amies@outlook.com>
2025-06-14 12:10:32 -0500

    -added logic for if user already exist

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,30 +9,29 @@ const app = express();
 app.use(express.json());
 const userRouter = Router();
 
-// //simple test route
-// userRouter.get('/test', (req, res) => res.send('Test route works!'));
-
 userRouter.post('/signup', async (req, res) => {
-    try{
-        //encrypts pw to store in the db 10 = number of salt
+    try {
+        // Check if user already exists
+        const existingUser = await User.findOne({ email: req.body.email });
+        if (existingUser) {
+            return res.json({ error: 'User already exists' });
+        }
+        // Encrypt password to store in the db
         const passwordHash = bcrypt.hashSync(req.body.password, 10);
         const newUser = new User({
-            //uses spread operator to add the rest of the the object for user
-            ...req.body, 
+            ...req.body,
             password: passwordHash
-        });   
+        });
         await newUser.save();
-        //creates a sessionToken that is unique to one person _id and JWT Secret
+        // Create a sessionToken that is unique to one person _id and JWT Secret
         const sessionToken = jwt.sign(
-            {_id: newUser._id},
+            { _id: newUser._id },
             process.env.JWT_SECRET,
-            //expires in 1 day
-                { expiresIn: 60 *60 *24 }
+            { expiresIn: 60 * 60 * 24 }
         );
-        res.json ({ message: 'Sign up successful', sessionToken});
-     
+        res.json({ message: 'Sign up successful', sessionToken });
     } catch (err) {
-        res.json({error: err});
+        res.json({ error: err });
         console.log(err);
     }
 });
